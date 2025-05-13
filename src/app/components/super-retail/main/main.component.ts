79ce84d3906b2749e2d05f11e9f54feeb4d6c308
jessPragma <jessica.rivero@pragma.com.co>
2025-05-13 08:54:08 -0500

    avances en correcciones solicitadas

--- src/app/components/super-retail/main/main.component.ts
+++ src/app/components/super-retail/main/main.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
+import { NgIf } from '@angular/common';
 import { ACardLocationComponent } from '@ui/atoms/a-card-location/a-card-location.component';
 import { ALogoComponent } from '@ui/atoms/a-logo/a-logo.component';
 import { MBannerOlimpicaComponent } from '@ui/molecules/m-banner-olimpica/m-banner-olimpica.component';
@@ -12,17 +13,36 @@ import { FooterSuperRetailComponent } from "../footer-super-retail/footer-super-
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [RouterOutlet, HeaderSuperRetailComponent, FooterSuperRetailComponent, TModalComponent, MNavbarOlimpicaComponent, ACardLocationComponent, ALogoComponent, OCarouselOlimpicaComponent, MBannerOlimpicaComponent],
+  imports: [RouterOutlet, HeaderSuperRetailComponent, FooterSuperRetailComponent, TModalComponent, MNavbarOlimpicaComponent, ACardLocationComponent, ALogoComponent, OCarouselOlimpicaComponent, MBannerOlimpicaComponent, NgIf],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
   @Input() showModal = false;
+  isCheckoutRoute: boolean = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    // Check initial route
+    this.checkIfCheckoutRoute(this.router.url);
+    
+    // Subscribe to route changes
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.checkIfCheckoutRoute(event.url);
+      }
+    });
+  }
+
+  // Check if current URL is a checkout route
+  private checkIfCheckoutRoute(url: string): void {
+    this.isCheckoutRoute = url.includes('/checkout');
+  }
 
   openModal(): void {
 		this.showModal = true;
 	}
-
   
 	closeModal(): void {
 		this.showModal = false;
